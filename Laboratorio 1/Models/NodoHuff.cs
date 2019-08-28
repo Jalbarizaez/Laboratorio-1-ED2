fc5bca8749074e8ae759c5ed3bc5bef4894fab9a
Jalbarizaez <jalbarizaez@gmail.com>
2019-08-28 17:57:47 -0600

    Creacion nodo de arbol huffman

---
+++ Laboratorio 1/Models/NodoHuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_1.Models
+{
+    public class NodoHuff
+    {
+        public string Dato { get; set; }
+        public double Probabilidad { get; set; }
+        public NodoHuff Izquierda { get; set; }
+        public NodoHuff Derecha { get; set; }
+        public NodoHuff Padre { get; set; }
+
+        public NodoHuff (string dato, double probabilidad)
+        {
+            Dato = dato;
+            Probabilidad = probabilidad;
+            Izquierda = null;
+            Derecha = null;
+            Padre = null;
+        }
+    }
+}
