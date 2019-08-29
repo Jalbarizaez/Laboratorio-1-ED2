478896a93b9186749c7e0044d479030d94f2c605
Jalbarizaez <jalbarizaez@gmail.com>
2019-08-28 19:02:57 -0600

    Creacion del Arbol Huffman

---
+++ Laboratorio 1/Models/CompresionHuffman.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio_1.Models
+{
+    public class CompresionHuffman
+    {
+        private static NodoHuff Raiz { get; set; }
+        public static Dictionary<string,string> Tabla_Caracteres { get; set; }
+        public static int Tamaño_Datos { get; set; }
+        public static double Cantidad_Datos;
+
+        public static byte[] Compresion (string Dato)
+        {
+            
+            Tabla_Caracteres = new Dictionary<string, string>();
+            ArbolHuffman(Dato);
+
+        }
+        public static NodoHuff Unir_Nodos(NodoHuff Mayor, NodoHuff Menor)
+        {
+            NodoHuff Padre = new NodoHuff('`',Mayor.Probabilidad+Menor.Probabilidad);
+            Padre.Izquierda = Mayor;
+            Padre.Derecha = Menor;
+            return Padre;
+        }
+        static void ArbolHuffman(string Dato)
+        {
+            Dictionary<char, int> Tabla_Frecuencias = new Dictionary<char, int>();
+            char[] Caracteres = Dato.ToCharArray();
+            Cantidad_Datos = Caracteres.Length;
+            Tamaño_Datos = Caracteres.Length;
+            foreach (char Caracter in Caracteres)
+            {
+                if (Tabla_Frecuencias.Keys.Contains(Caracter))
+                {
+                    Tabla_Frecuencias[Caracter] = Tabla_Frecuencias[Caracter]++;
+                }
+                else Tabla_Frecuencias.Add(Caracter, 1);
+            }
+            List<NodoHuff> Lista_Frecuencias = new List<NodoHuff>();
+            foreach(KeyValuePair<char,int> Nodos in Tabla_Frecuencias)
+            {
+                Lista_Frecuencias.Add(new NodoHuff(Nodos.Key, (Convert.ToDouble(Nodos.Value) / Cantidad_Datos)));
+            }
+            while(Lista_Frecuencias.Count >1)
+            {
+                Lista_Frecuencias.OrderBy(x => x.Probabilidad);
+                NodoHuff Union = Unir_Nodos(Lista_Frecuencias[1], Lista_Frecuencias[0]);
+                Lista_Frecuencias.RemoveRange(0, 2);
+            }
+            Raiz = Lista_Frecuencias[0];
+        }
+    }
+}
--- Laboratorio 1/Models/NodoHuff.cs
+++ Laboratorio 1/Models/NodoHuff.cs
@@ -7,19 +7,24 @@ namespace Laboratorio_1.Models
 {
     public class NodoHuff
     {
-        public string Dato { get; set; }
+        public char Dato { get; set; }
         public double Probabilidad { get; set; }
         public NodoHuff Izquierda { get; set; }
         public NodoHuff Derecha { get; set; }
         public NodoHuff Padre { get; set; }
 
-        public NodoHuff (string dato, double probabilidad)
+        public NodoHuff (char dato, double probabilidad)
         {
             Dato = dato;
             Probabilidad = probabilidad;
             Izquierda = null;
             Derecha = null;
             Padre = null;
+        }
+        public bool HoJa()
+        {
+            if (Derecha == null && Izquierda == null) return true;
+            else return false;
         }
     }
 }
