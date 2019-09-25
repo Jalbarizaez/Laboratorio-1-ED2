5020115ab05731b6832bc9ec9a5c36d3fd4c4881
Jalbarizaez <jalbarizaez@gmail.com>
2019-09-24 18:47:14 -0600

    Arreglando commit

--- Laboratorio 1/Models/CompresionLZW.cs
+++ Laboratorio 1/Models/CompresionLZW.cs
@@ -16,7 +16,7 @@ namespace Laboratorio_1.Models
             Escribir_Diccionario(pathEscritura);
             Diccionario_Completo(pathLectura, pathEscritura);
         }
-<<<<<<< HEAD
+
         private Dictionary<string, int> Tabla_Caracteres = new Dictionary<string, int>();
         private Dictionary<string, int> Tabla_Escritura = new Dictionary<string, int>();
         private const int bufferLenght = 500;
@@ -95,49 +95,12 @@ namespace Laboratorio_1.Models
 
             //Escribir aqui el diccionario al archivo
         }
-=======
-		private void Diccionario_Inicial(string pathLectura)
-		{
-			var buffer = new char[bufferLenght];	
-			//Se llena el diccionario con los valores iniciales
-			using (var file = new FileStream(pathLectura, FileMode.Open))
-			{
-				using (var reader = new BinaryReader(file))
-				{
-					while (reader.BaseStream.Position != reader.BaseStream.Length)
-					{
-						buffer = reader.ReadChars(bufferLenght);
-						foreach (var item in buffer)
-						{
-							if (!Tabla_Caracteres.ContainsKey(Convert.ToString(item)) && Convert.ToInt16(item) != 13)
-							{
-								if (Tabla_Caracteres.Count() == 0)
-									Tabla_Caracteres.Add(Convert.ToString(item), 1);
-								else
-									Tabla_Caracteres.Add(Convert.ToString(item), (Tabla_Caracteres.Count + 1));
-							}
-						}
-					}
-				}
-			}
-		}
->>>>>>> 5fd54198459c9d165fe0536ef373f8c7f1d38920
+
         private void Escribir_Diccionario(string pathEscritura)
         {
             char separador = '|';
             var escritura = new byte[bufferLenght];
-<<<<<<< HEAD
 
-            if (Tabla_Caracteres.Keys.Contains("|"))
-            {
-                separador = 'ÿ';
-                if (Tabla_Caracteres.Keys.Contains("ÿ"))
-                {
-                    separador = 'ß';
-                }
-            }
-
-=======
 			if (Tabla_Caracteres.Keys.Contains("|"))
 			{
 				separador = 'ÿ';
@@ -146,7 +109,7 @@ namespace Laboratorio_1.Models
 					separador = 'ß';
 				}
 			}
->>>>>>> 5fd54198459c9d165fe0536ef373f8c7f1d38920
+
             using (var file = new FileStream(pathEscritura, FileMode.OpenOrCreate))
             {
                 using (var writer = new BinaryWriter(file))
@@ -169,7 +132,7 @@ namespace Laboratorio_1.Models
 
         }
         private void Diccionario_Completo(string pathLectura, string pathEscritura)
-<<<<<<< HEAD
+
         {
             var buffer = new byte[bufferLenght];
             var resultado = new List<int>();
@@ -206,37 +169,5 @@ namespace Laboratorio_1.Models
             //Escribir aca todo los numero en bytes al archivo de escritura
         }
     }
-=======
-		{
-			var buffer = new char[bufferLenght];
-			var resultado = new List<int>();
-			string UltimoCaracter = "";
-			using (var file = new FileStream(pathLectura, FileMode.Open))
-			{
-				using (var reader = new BinaryReader(file))
-				{
-					while (reader.BaseStream.Position != reader.BaseStream.Length)
-					{
-						buffer = reader.ReadChars(bufferLenght);
-						foreach (var item in buffer)
-						{
-							if (!Tabla_Caracteres.ContainsKey((UltimoCaracter + Convert.ToString(Convert.ToChar(item)))))
-							{
-								Tabla_Caracteres.Add((UltimoCaracter + Convert.ToString(Convert.ToChar(item))), (Tabla_Caracteres.Count + 1));
-								resultado.Add(Tabla_Caracteres[UltimoCaracter]);
-								UltimoCaracter = (UltimoCaracter + Convert.ToString(Convert.ToChar(item))).Substring(((UltimoCaracter + Convert.ToString(Convert.ToChar(item))).Length - 1), 1);
-							}
-							else
-							{
-								UltimoCaracter = (UltimoCaracter + Convert.ToString(Convert.ToChar(item)));
-							}
-						}
-						resultado.Add(Tabla_Caracteres[UltimoCaracter]);
-						int mayor = resultado.Max();
-					}
-				}
-			}
-		}
-	}
->>>>>>> 5fd54198459c9d165fe0536ef373f8c7f1d38920
+
 }
